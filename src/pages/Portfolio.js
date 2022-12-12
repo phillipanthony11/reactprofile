23e8d7454ad4588475a55749df803e9f98aca058
Sean Anthony <sammybanthony1@gmail.com>
2022-12-12 13:08:46 -0800

    some more

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -9,16 +9,45 @@ export default function Portfolio() {
         Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
         dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
         sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
       </p>
+      <div className="work">
+        {/* <div className="other-work"> */}
+         
+          <a href="http://www.github.com" target="_blank" className="proj-link">
+             Project 1
+          </a>
+        {/* </div> */}
+        <div className="other-work">
+          
+          <a href="http://www.github.com" target="_blank" className="proj-link">
+             Project 2
+          </a>
+        </div>
+        <div className="other-work">
+          
+          <a href="http://www.github.com" target="_blank" className="proj-link">
+             Project 3
+          </a>
+        </div>
+        <div className="other-work">
+          
+          <a href="http://www.github.com" target="_blank" className="proj-link">
+             Project 4
+          </a>
+        </div>
+        <div className="other-work">
+          
+          <a href="http://www.github.com" target="_blank" className="proj-link">
+             Project 5
+          </a>
+        </div>
+        <div className="other-work">
+  
+          <a href="http://www.github.com" target="_blank" className="proj-link">
+             Project 6
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
